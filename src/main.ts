cf1ee2376d8f27b835816147dbcbe4db6ff7066a
MAX4020 <144027580+MAX4020@users.noreply.github.com>
2024-06-03 17:56:35 +0400

    работа с nestjs
    проект

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { AppModule } from "./app.module";
+import "dotenv/config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
 }
 bootstrap();
+
+// Баги в ТЗ общее:
+
+/* BUG ТЗ: функциональность без регистрации:
+  1. не известен endpoint главной страницы, по корню страницы не грузятся никакие заголовки
+  2. где отображать желающих не ясно.
+*/
